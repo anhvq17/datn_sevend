76222cf89f661a768ae8c5242fba11e929907bbe
Vu Quoc Anh <hinm.aaaa@gmail.com>
2025-05-17 01:30:44 +0700

    connect homepage & dashboard

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -1,20 +1,23 @@
 import { useRoutes } from 'react-router-dom'
 
 import ClientLayout from './layouts/ClientLayout';
+import Homepage from './pages/Client/Homepage';
 import Login from './pages/Auth/Login';
 import Register from './pages/Auth/Register';
 
 import AdminLayout from './layouts/AdminLayout';
+import Dashboard from './pages/Admin/Dashboard';
 
 function App() {
   const routes = useRoutes([
     { path: '/', element: <ClientLayout />, children: [
-      { path: 'login', element: <Login />},
-      { path: 'register', element: <Register />},
+      { path: '', element: <Homepage /> },
+      { path: 'login', element: <Login /> },
+      { path: 'register', element: <Register /> },
     ] },
 
     { path: '/dashboard', element: <AdminLayout />, children: [
-
+      { path: '', element: <Dashboard /> },
     ] },
   ])
   return routes;
