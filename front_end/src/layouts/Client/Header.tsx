5e57a52b84d16ddde65c84b074148a9f6058b370
Vu Quoc Anh <hinm.aaaa@gmail.com>
2025-05-17 20:48:56 +0700

    tao header & footer phia client

--- front_end/src/layouts/Client/Header.tsx
+++ front_end/src/layouts/Client/Header.tsx
@@ -1,8 +1,43 @@
+import { Link } from "react-router-dom"
+
 const ClientHeader = () => {
   return (
-    <div>
-      ClientHeader
-    </div>
+    <header className="w-full h-[60px] bg-white shadow-md">
+      <div className="max-w-[1280px] mx-auto px-6 h-full flex items-center justify-between">
+        <div className="h-full flex items-center">
+          <Link to={"/"}>
+            <img src="/img/SEVEND.png" alt="Logo" className="h-6 object-contain" />
+          </Link>
+        </div>
+
+        <nav className="flex items-center space-x-8 text-sm font-medium uppercase">
+          <div className="relative group cursor-pointer">
+            <span className="hover:text-gray-700">Nam <i className="fas fa-angle-down ml-1"></i></span>
+          </div>
+          <div className="relative group cursor-pointer">
+            <span className="hover:text-gray-700">Nữ <i className="fas fa-angle-down ml-1"></i></span>
+          </div>
+          <div className="relative group cursor-pointer">
+            <span className="hover:text-gray-700">Thương hiệu <i className="fas fa-angle-down ml-1"></i></span>
+          </div>
+        </nav>
+
+        <div className="flex items-center space-x-6 text-xl text-black">
+          <div className="relative">
+            <input type="text" placeholder="Tìm kiếm" className="border border-gray-300 rounded-full px-4 py-1 text-sm focus:outline-none focus:ring-1 focus:ring-black" />
+            <i className="fas fa-search absolute right-3 top-1 text-base"></i>
+          </div>
+          <div className="h-5 border-l border-gray-300"></div>
+          <Link to={"/login"} className="hover:text-gray-600">
+            <i className="fas fa-user text-base"></i>
+          </Link>
+          <div className="h-5 border-l border-gray-300"></div>
+          <Link to={"/login"} className="hover:text-gray-600">
+            <i className="fas fa-cart-shopping text-base"></i>
+          </Link>
+        </div>
+      </div>
+    </header>
   )
 }
 
